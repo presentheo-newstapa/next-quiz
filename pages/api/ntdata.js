047cd88b2ac5e2c4ad118f9d1d124ee518d4c617
jae18067 <jae18067@gmail.com>
2021-10-13 18:40:47 +0900

    불필요한 컴포넌트 통합하고 정리

---
+++ pages/api/ntdata.js
@@ -0,0 +1,48 @@
+const NTdataSet = [
+  {
+    name: '김지윤',
+    mbti: 'ISTJ',
+  },
+  {
+    name: '연다혜',
+    mbti: 'ISFJ',
+  },
+  {
+    name: '허현재',
+    mbti: 'ISFJ',
+  },
+  {
+    name: '정은아',
+    mbti: 'ISFJ',
+  },
+  {
+    name: '이도현',
+    mbti: 'INFJ',
+  },
+  {
+    name: '최윤원',
+    mbti: 'INFJ',
+  },
+  {
+    name: '조원일',
+    mbti: 'INFJ',
+  },
+  {
+    name: '이명주',
+    mbti: 'INFJ',
+  },
+  {
+    name: '오대양',
+    mbti: 'INFJ',
+  },
+  {
+    name: '김성근',
+    mbti: 'INFJ',
+  },
+  {
+    name: '송원근',
+    mbti: 'INTJ',
+  },
+]
+
+export default NTdataSet;
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,71 @@
 import React, {useState, useEffect} from 'react';
+import _ from 'lodash';
 import Head from 'next/head';
-import User from '../components/user';
-import Quiz from '../components/quiz';
-import Result from '../components/result';
+import Button from '../components/button';
+import dataSet from '../pages/api/data';
 
 export default function Home(props) {
 
-  const [user, setUser] = useState({
-    state: null,
-    name: null,
-    job: null    
-  });
+  const [userState, setUserState] = useState(0);
+  const [userName, setUserName] = useState('');
+  const [userJob, setUserJob] = useState('기자');
+
+  const [progress, setProgress] = useState(0);
+  const [selection, setSelection] = useState(null);
+  const [selectionList, setSelectionList] = useState([]);
+
+  let currentData = dataSet[progress]
+
+  function enterUserInfo(){
+    if (userName.length < 1) {
+      alert('이름을 한 글자 이상 입력해주세요!')
+    } else {
+      alert(`${userName} ${userJob}, 뉴스타파에 오신 것을 축하합니다!`)
+      setUserState(1);
+    }
+  }
+
+    // 답변 선택하기
+  function selectAnswer(index) {
+    setSelection(currentData.answers[index])
+  }
+
+  // 다음으로 가기 & 뒤로 가기
+  function goTo(direction){
+    if (direction == 'prev') {
+      setSelectionList(_.dropRight(selectionList))
+      setProgress(progress==0?progress:progress-1)
+      setSelection(null)
+    }
+    if (direction == 'next') {
+      if (selection == null) {
+        alert('한 개 이상 선택해주세요')
+      } else {
+        setSelectionList([...selectionList, selection])
+        setProgress(progress==dataSet.length-1?progress:progress+1)
+        setSelection(null)
+      }
+    }
+  }
+
+  // 제출하기
+  function submit(){
+    if (selection == null) {
+      alert('한 개 이상 선택해주세요')
+    } else {
+      const final = [...selectionList, selection]
+      // ajax 통신으로 답변 제출하고 채점하기
+      if (window.confirm('제출하시겠어요?')){
+        setUserState(2);
+        console.log(final);
+      }
+    }
+  }
+
+  // 이펙트
+  useEffect(() => {
+    selectAnswer;
+  }, [selection])
 
   return (
     <div>
@@ -20,12 +75,83 @@ export default function Home(props) {
       </Head>
 
       <main>
-        <div>
+        {/* 이름 입력하는 부분 */}
+        {userState == 0 && (
+          <div className="container max-w-screen-md px-20 py-28 mx-auto">
+            <div>
+              <h1 className="md:text-4xl font-black text-center">재미로 하는 테스트!<br/>당신과 닮은 뉴타人은 누구?!?</h1>
+            </div>
+            <div className="mt-10">
+              <label htmlFor="userName" className="sr-only">
+                이름
+              </label>
+              <div className="mt-1 relative rounded-md shadow-sm">
+                <input
+                  onChange={(e) => setUserName(e.target.value)}
+                  type="text"
+                  name="userName"
+                  id="inputUserName"
+                  className="focus:ring-indigo-500 focus:border-indigo-500 block w-full pl-7 py-7 pr-20 md:text-xl sm:text-lg border-gray-300 rounded-md"
+                  placeholder="이름"
+                />
+                <div className="absolute inset-y-0 right-0 flex items-center">
+                  <label htmlFor="userJob" className="sr-only">
+                    직책
+                  </label>
+                  <select
+                    onChange={(e) => setUserJob(e.target.value)}
+                    id="userJob"
+                    name="inputUserJob"
+                    className="focus:ring-indigo-500 focus:border-indigo-500 h-full py-0 pl-2 border-transparent bg-transparent text-gray-500 md:text-xl sm:text-lg rounded-md"
+                  >
+                    <option value="기자">기자</option>
+                    <option value="PD">PD</option>
+                  </select>
+                </div>
+              </div>
+            </div>
+            <div className="mt-4">
+              <Button onClick={() => enterUserInfo()} classList="text-indigo-700 bg-indigo-100 hover:bg-indigo-200">시작하기!</Button>
+            </div>
+          </div>
 
-        </div>
-        <User></User>
-        <Quiz></Quiz>
-        <Result></Result>
+        )}
+        {userState == 1 && (
+          <div className="container max-w-screen-md px-20 py-28 mx-auto">
+            
+            {/* 퀴즈 창 */}
+            <div className="quiz">
+              <div>
+                {/* 질문 영역 */}
+                <div>
+                  <h3 className="text-2xl">{currentData.question}</h3>
+                </div>
+                {/* 답변 리스트 영역 */}
+                <div>
+                  <ul>
+                    {currentData.answers.map((e, i) => {
+                      return <li className={`mt-4 px-6 py-4 border rounded cursor-pointer ${selection==e?'bg-indigo-100 shadow-inner':''}`} onClick={(event) => selectAnswer(i)} key={i}>{e.content}</li>
+                    })}
+                  </ul>
+                </div>
+              </div>
+              <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
+                {progress > 0 ? <Button classList="text-indigo-700 bg-indigo-100 hover:bg-indigo-200 " onClick={(event) => goTo('prev')}>이전으로 가기</Button> : ''}
+                {progress < dataSet.length-1 ? <Button classList="text-white bg-indigo-700 hover:bg-indigo-800 ml-2" onClick={(event) => goTo('next')}>다음으로 가기</Button> : <Button classList="text-white bg-indigo-700 hover:bg-indigo-800 ml-2" onClick={(event) => submit()}>제출하기</Button>}
+              </div>
+            </div>
+
+          </div>
+        )}
+
+        {userState == 2 && (
+          <div className="container max-w-screen-md px-20 py-28 mx-auto">
+            <div>
+              <p>당신과 닮은 기자는 김용진 대표기자 입니다</p>
+            </div>
+
+          </div>
+        )}
       </main>
 
     </div>
