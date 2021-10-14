ea2166828bb8cff792c80cd75b4e48bb8fccb4d8
jae18067 <jae18067@gmail.com>
2021-10-14 18:24:19 +0900

    스타일 수정

--- pages/api/data.js
+++ pages/api/data.js
@@ -3,48 +3,48 @@ const dataSet = [
     id: 1,
     question: '뉴스타파에 입사한 당신, 첫 취재를 나가게 됐다! 당신이 취재하고 싶은 아이템은?',
     answers: [
-      {content: '어려운 사람들의 가슴 아픈 이야기', isCorrect: false},
-      {content: '권력기관의 비리를 파헤치는 보도', isCorrect: false}
+      {content: '평범한 사람들의 가슴 아픈 이야기', value: false},
+      {content: '권력기관의 비리를 파헤치는 보도', value: false}
     ]
   },
   {
     id: 2,
     question: '취재할 아이템을 정했다. 생각보다 취재가 어려울 것 같은데... 팀을 이뤄서 취재해볼까?',
     answers: [
-      {content: '혼자가 더 편하다.', isCorrect: false},
-      {content: '여럿이서 힘을 합쳐 보자', isCorrect: true},
+      {content: '혼자가 더 편하다.', value: false},
+      {content: '여럿이서 힘을 합쳐 보자', value: true},
     ]
   },
   {
     id: 3,
     question: '취재를 하던 중 난관에 부딪쳤다. 어떻게 해결할까?',
     answers: [
-      {content: '자료를 살펴본다', isCorrect: false},
-      {content: '내 직관을 믿는다', isCorrect: true},
+      {content: '자료를 살펴본다', value: false},
+      {content: '내 직관을 믿는다', value: true},
     ]
   },
   {
     id: 4,
     question: '생각보다 취재에 시간이 오래 걸렸다. 마감 시간에 맞추지 못할 수도 있을 것 같다. 어떻게 할까?',
     answers: [
-      {content: '미리 양해를 구하고 마감 시간을 늦춘다', isCorrect: false},
-      {content: '어떻게든 최선을 다해본다', isCorrect: true},
+      {content: '미리 양해를 구하고 마감 시간을 늦춘다', value: false},
+      {content: '어떻게든 최선을 다해본다', value: true},
     ]
   },
   {
     id: 5,
     question: '다행히 제 시간에 기사를 마감했다. 내일은 내 첫 기사가 공개되는 날! 지금 기분은?',
     answers: [
-      {content: '두근두근, 긴장돼서 잠이 안 온다 😥', isCorrect: false},
-      {content: '최선을 다했으니 후회도 없다. 내일 일은 내일 생각하자!', isCorrect: true},
+      {content: '두근두근, 긴장돼서 잠이 안 온다 😥', value: false},
+      {content: '최선을 다했으니 후회도 없다. 내일 일은 내일 생각하자!', value: true},
     ]
   },
   {
     id: 6,
     question: '드디어 첫 기사가 공개됐다. 그동안 수고한 내 자신에게 ',
     answers: [
-      {content: '선배들과 함께 뒷풀이 모임을 갖는다', isCorrect: false},
-      {content: '조용히 혼자만의 시간을 보낸다', isCorrect: true},
+      {content: '선배들과 함께 뒷풀이 모임을 갖는다', value: false},
+      {content: '조용히 혼자만의 시간을 보낸다', value: true},
     ]
   },
 ]
--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home(props) {
 
   // 유저 상태 관리
   const [userState, setUserState] = useState(0);
-  const [userName, setUserName] = useState('');
+  const [userName, setUserName] = useState('뉴스타파');
   const [userJob, setUserJob] = useState('기자');
 
   // 퀴즈 상태 관리
@@ -23,7 +23,6 @@ export default function Home(props) {
     if (userName.length < 1) {
       alert('이름을 한 글자 이상 입력해주세요!')
     } else {
-      alert(`${userName} ${userJob}, 뉴스타파에 오신 것을 축하합니다!`)
       setUserState(1);
     }
   }
@@ -77,107 +76,119 @@ export default function Home(props) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        {/* 이름 입력창 */}
-        {userState == 0 && (
-          <div className="container max-w-screen-md px-20 py-28 mx-auto">
+      <main className="min-h-screen flex items-center bg-nstp">
+        <div className="container max-w-screen-sm mx-auto px-5 pt-5 pb-10">
+          {/* 이름 입력창 */}
+          {userState == 0 && (
             <div>
-              <h1 className="md:text-4xl font-black text-center">재미로 하는 테스트!<br/>당신과 닮은 뉴타人은 누구?!?</h1>
-            </div>
-            <div className="mt-10">
-              <label htmlFor="userName" className="sr-only">
-                이름
-              </label>
-              <div className="mt-1 relative rounded-md shadow-sm">
-                <input
-                  onChange={(e) => setUserName(e.target.value)}
-                  type="text"
-                  name="userName"
-                  id="inputUserName"
-                  className="focus:ring-indigo-500 focus:border-indigo-500 block w-full pl-7 py-7 pr-20 md:text-xl sm:text-lg border-gray-300 rounded-md"
-                  placeholder="이름"
-                />
-                <div className="absolute inset-y-0 right-0 flex items-center">
-                  <label htmlFor="userJob" className="sr-only">
-                    직책
-                  </label>
-                  <select
-                    onChange={(e) => setUserJob(e.target.value)}
-                    id="userJob"
-                    name="inputUserJob"
-                    className="focus:ring-indigo-500 focus:border-indigo-500 h-full py-0 pl-2 border-transparent bg-transparent text-gray-500 md:text-xl sm:text-lg rounded-md"
-                  >
-                    <option value="기자">기자</option>
-                    <option value="PD">PD</option>
-                  </select>
-                </div>
-              </div>
-            </div>
-            <div className="mt-4">
-              <Button onClick={() => enterUserInfo()} classList="text-indigo-700 bg-indigo-100 hover:bg-indigo-200">시작하기!</Button>
-            </div>
-          </div>
-
-        )}
-
-        {/* 퀴즈 시작 */}
-        {userState == 1 && (
-          <div className="container max-w-screen-md px-20 py-28 mx-auto">
-            <div className="quiz">
               <div>
-                {/* 질문 영역 */}
-                <div>
-                  <h3 className="text-2xl">{currentData.question}</h3>
-                </div>
-                {/* 답변 리스트 영역 */}
-                <div>
-                  <ul>
-                    {currentData.answers.map((e, i) => {
-                      return <li className={`mt-4 px-6 py-4 border rounded cursor-pointer ${selection==e?'bg-indigo-100 shadow-inner':''}`} onClick={(event) => selectAnswer(i)} key={i}>{e.content}</li>
-                    })}
-                  </ul>
+                <h1 className="text-2xl text-white text-center font-black md:leading-normal">재미로 하는 테스트!<br/>당신과 닮은 뉴타人은 누구?!?🤣</h1>
+              </div>
+              <div className="mt-10">
+                <label htmlFor="userName" className="sr-only">
+                  이름
+                </label>
+                <div className="mt-1 relative rounded-md shadow-sm">
+                  <input
+                    onChange={(e) => setUserName(e.target.value)}
+                    type="text"
+                    name="userName"
+                    id="inputUserName"
+                    className="focus:ring-indigo-500 focus:border-indigo-500 block w-full pl-7 py-7 pr-20 md:text-xl sm:text-lg border-gray-300 rounded-md"
+                    placeholder="이름"
+                  />
+                  <div className="absolute inset-y-0 right-0 flex items-center">
+                    <label htmlFor="userJob" className="sr-only">
+                      직책
+                    </label>
+                    <select
+                      onChange={(e) => setUserJob(e.target.value)}
+                      id="userJob"
+                      name="inputUserJob"
+                      className="focus:ring-indigo-500 focus:border-indigo-500 h-full py-0 pl-2 border-transparent bg-transparent text-gray-500 md:text-xl sm:text-lg rounded-md"
+                    >
+                      <option value="기자">기자</option>
+                      <option value="PD">PD</option>
+                    </select>
+                  </div>
                 </div>
               </div>
-              <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
-                {progress > 0 ? <Button classList="text-indigo-700 bg-indigo-100 hover:bg-indigo-200 " onClick={(event) => goTo('prev')}>이전으로 가기</Button> : ''}
-                {progress < dataSet.length-1 ? <Button classList="text-white bg-indigo-700 hover:bg-indigo-800 ml-2" onClick={(event) => goTo('next')}>다음으로 가기</Button> : <Button classList="text-white bg-indigo-700 hover:bg-indigo-800 ml-2" onClick={(event) => submit()}>제출하기</Button>}
+              <div className="mt-4">
+                <Button onClick={() => enterUserInfo()} classList="text-indigo-700 bg-indigo-100 hover:bg-indigo-200">시작하기!</Button>
               </div>
             </div>
+          )}
 
-          </div>
-        )}
-
-        {/* 결과 출력 창 */}
-        {userState == 2 && (
-          <div className="container max-w-screen-md px-20 py-28 mx-auto">
+          {/* 퀴즈 시작 */}
+          {userState == 1 && (
             <div>
-              <p>당신과 닮은 기자는...</p>
+              <div className="quiz">
+                <div>
+                  {/* 코멘트 영역 */}
+                  <div className="py-4 px-6 mb-4 rounded bg-gray-100 flex flex-row gap-3">
+                    <div className="w-20 h-20 overflow-hidden rounded-full border">
+                      <img className="object-contain" src="/images/muckracker.jpg"></img>
+                    </div>
+                    {progress == 0 && (
+                      <p className="text-md">{`${userName} ${userJob}, 뉴스타파에 온 것을 환영해요. 첫 취재 기대하고 있겠습니다.`}</p>
+                    )}
+                  </div>
+                  {/* 질문 영역 */}
+                  <div>
+                    <p className="text-2xl leading-normal">{currentData.question}</p>
+                  </div>
+                  {/* 답변 리스트 영역 */}
+                  <div>
+                    <ul>
+                      {currentData.answers.map((e, i) => {
+                        return <li className={`mt-4 px-6 py-4 border rounded cursor-pointer ${selection==e?'bg-indigo-100 shadow-inner':''}`} onClick={(event) => selectAnswer(i)} key={i}>{e.content}</li>
+                      })}
+                    </ul>
+                  </div>
+                </div>
+                <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
+                  {progress > 0 ? <Button classList="text-indigo-700 bg-indigo-100 hover:bg-indigo-200 " onClick={(event) => goTo('prev')}>이전으로 가기</Button> : ''}
+                  {progress < dataSet.length-1 ? <Button classList="text-white bg-indigo-700 hover:bg-indigo-800 ml-2" onClick={(event) => goTo('next')}>다음으로 가기</Button> : <Button classList="text-white bg-indigo-700 hover:bg-indigo-800 ml-2" onClick={(event) => submit()}>제출하기</Button>}
+                </div>
+              </div>
+
             </div>
+          )}
+
+          {/* 결과 출력 창 */}
+          {userState == 2 && (
             <div>
-              <ul className="grid grid-cols-4 gap-2">
-                {NTdataSet.map((e, i) => {
-                  if (e.mbti == "ISFJ") {
-                    return (
-                      <li>
-                        <div>
-                          <div className="w-24 h-24 overflow-hidden rounded-full mx-auto border">
-                            <img className="object-contain" src={'/images/'+e.id+'.jpg'}></img>
+              <div>
+                <h2 className="md:text-4xl font-black text-center">당신과 닮은 뉴타人은...</h2>
+              </div>
+              <div className="mt-10">
+                <ul className="grid grid-cols-4 gap-2">
+                  {NTdataSet.map((e, i) => {
+                    // 결과가 일치하는 데이터 불러오기
+                    if (e.mbti == "ISFJ") {
+                      return (
+                        <li>
+                          <div>
+                            <div className="w-24 h-24 overflow-hidden rounded-full mx-auto border">
+                              <img className="object-contain" src={'/images/'+e.id+'.jpg'}></img>
+                            </div>
+                            <h4 className="text-center">{e.name} </h4>
+
                           </div>
-                          <h4 className="text-center">{e.name} </h4>
+                        </li>
+                      )
+                    }
+                  })}
+                </ul>
+              </div>
+              <div>
+                <p>ISFJ 유형 성격의 소유자는 조용하고 차분하며 따뜻하고 친근하다. 책임감과 인내력 또한 매우 강하다. 본인의 친한 친구나 가족에게 애정이 가득하다. 이들은 언제나 진솔하려 노력하고 가볍지 않기 때문에 관계를 맺기에 가장 믿음직스러운 유형이다.<br/>사회생활 시 외부 환경에 대해 내향형 중에서 가장 방어력이 강하다. 감정을 파악하는 데는 능숙하지만 표현하는 데는 서툴기 때문에 관계에 있어서 걱정을 하는 경우가 많다. 실제적이고 꼼꼼하게 계획적이며 협조적으로 일을 처리한다. 완벽한 결과물을 도출하지 못할 경우 스트레스를 상당히 받으며, 이상과 달리 귀차니즘이 생겼을 시, 자신에게서도 극심한 괴리감을 느낀다.[4] 경험을 통해서 자신이 틀렸다고 인정할 때까지 꾸준히 밀고 나가는 편이다.</p>
 
-                        </div>
-                      </li>
-                    )
-                  }
-                })}
-              </ul>
-            </div>
-            <div>
-              <p>입니다</p>
-            </div>
+              </div>
 
-          </div>
-        )}
+            </div>
+          )}
+        </div>
       </main>
 
     </div>
